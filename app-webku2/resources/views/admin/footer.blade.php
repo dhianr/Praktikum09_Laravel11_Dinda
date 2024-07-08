ded7b56ae0cfc75b2495d147aa9650a0911c3f37
dhianr <nurfadhiad@gmail.com>
2024-07-08 10:14:44 +0700

    praktikum09

---
+++ app-webku2/resources/views/admin/footer.blade.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="float-right d-none d-sm-block">
+      <b>Praktikum 9</b> Pemrograman Web 2 - Laravel
+    </div>
+    <strong>Copyright &copy; 2024 <a href="https://linkedin.com/in/dinda-nurfadhia-73abb0249/">Dinda Nurfadhia</a>.</strong>
+  </footer>
